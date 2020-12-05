8aeed99c4134c2c0eccece8c6192d959ce5e315a
lacadev <36073068+lacadev@users.noreply.github.com>
2020-12-05 14:06:57 +0100

    Update homeip script

--- scripts/updatehomeip.sh
+++ scripts/updatehomeip.sh
@@ -5,11 +5,16 @@
 # sharing the note with your personal one if needed
 
 # Needs BW_SESSION in secrets.sh
-SCRIPTS_DIR="/opts/scripts"
+SCRIPTS_DIR="/opt/scripts"
 source "${SCRIPTS_DIR}/secrets.sh"
 
-BW_SESSION="${BW_SESSION}" /usr/local/bin/bw --quiet sync
-json_out=$(BW_SESSION="${BW_SESSION}" /usr/local/bin/bw get item "home-ip")
+# The --apikey flag is available from v1.13 onwards
+BW_CLIENTID="$BW_CLIENTID" BW_CLIENTSECRET="$BW_CLIENTSECRET" /usr/local/bin/bw login --apikey --quiet
+BW_SESSION="$(/usr/local/bin/bw unlock "$BW_PSWD" --raw)"
+export BW_SESSION="${BW_SESSION}"
+
+/usr/local/bin/bw --quiet sync
+json_out=$(/usr/local/bin/bw get item "home-ip")
 note_id=$(echo "$json_out" | jq --raw-output ".id")
 
 stored_ip=$(echo "$json_out" | jq --raw-output ".notes")
@@ -21,8 +26,8 @@ else
   echo "$json_out" | \
     jq '.notes = "'"${current_ip}"'"' | \
     /usr/local/bin/bw encode | \
-    BW_SESSION="${BW_SESSION}" /usr/local/bin/bw --quiet edit item ${note_id}
+    /usr/local/bin/bw --quiet edit item ${note_id}
   echo "IP has been updated"
 fi
 
-
+/usr/local/bin/bw logout --quiet
